88c3df8c51bcb8fb506859dc52ef9f2db78d06b5
kniedzie <kniedzie@d17-l427-pc-10.ipa.ki.agh.edu.pl>
2019-12-02 09:11:50 +0100

    buttony

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <h1>{{ title }} {{nr}} times</h1>
+    <h1>{{ title }} {{nr*nr}} times</h1>
     <input type="text" v-model="title">
+    <button @click="iAmClicked()">Click me!</button>
+    <button @click="reset()">reset</button>
     <h2>{{qwerty}}</h2>
     <button @click="iAmClicked()"><input type="text" v-model="qwerty"></button>
-    <button @click="iAmClicked()">Click me!</button>
+    <br>
+    <h2></h2><button @click="changeMood()">how are u?</button></div></h2>
   </div>
 </template>
 
@@ -13,14 +16,24 @@
     data() {
       return {
         title: 'PRYMUS',
-        qwerty: 'coś tam',
-        nr: 0
+        qwerty: 'happy',
+        nr: 0,
+        i: false
+
       }
     },
     methods: {
       iAmClicked() {
         this.title = 'Clicked';
-        nr = nr+1;
+        this.nr = this.nr + 1;
+
+      },
+      reset() {
+        this.nr = 0;
+      },
+      changeMood(){
+        this.i = ! this.i;
+        this.qwerty = this.i ? 'happy' : 'sad';
       }
     }
   };
